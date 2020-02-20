34a80e3c06f8cf226cbd0478499974e09ffc2356
Литвиненко Юрий <litvinenko-don@ukr.net>
2020-02-20 20:35:10 +0300

    added animation for homepage

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-"use strict";
+//"use strict";
 
 var gulp = require("gulp");
 var sass = require("gulp-sass");
@@ -11,7 +11,7 @@ var csso = require("gulp-csso");
 var rename = require("gulp-rename");
 var imagemin = require("gulp-imagemin");
 var webp = require("gulp-webp");
-//var svgstore = require("gulp-svgstore");
+var svgstore = require("gulp-svgstore");
 var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
 var del = require("del");
