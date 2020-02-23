9efe326782654f06fe2da1ff0c57a8e919f39693
Литвиненко Юрий <litvinenko-don@ukr.net>
2020-02-23 14:56:38 +0300

    added .me + .me__list - scss + js

--- source/js/script.js
+++ source/js/script.js
@@ -1,12 +1,18 @@
 "use strict";
 
-let me = document.querySelector('.me');
 let contact = document.querySelector('.contact');
+let me = document.querySelector('.me');
+
 let contactHeaderWrap = document.querySelector('.contact__header-wrap');
+let meHeaderWrap = document.querySelector('.me__header-wrap');
+
 let contactHeader = document.querySelector('.contact__header');
+let meHeader = document.querySelector('.me__header');
+
 let contactListWrap = document.querySelector('.contact__list-wrap');
+let meListWrap = document.querySelector('.me__list-wrap');
 
-
+/*====*/
 contactHeaderWrap.addEventListener('click', function () {
   me.classList.toggle('me--none');
   contact.classList.toggle('contact--width');
@@ -16,24 +22,58 @@ contactHeaderWrap.addEventListener('click', function () {
 
 contactHeaderWrap.addEventListener('mouseover', function () {
   if (contactHeaderWrap.classList.contains('contact__header-wrap--width') === true) {
-    addBACK();
+    addBackRight();
   } else {
-    addCONTACT();
+    addContactRight();
   }
 });
 
 contactHeaderWrap.addEventListener('mouseout', function () {
   if (contactHeaderWrap.classList.contains('contact__header-wrap--width') === true) {
-    addCONTACT();
+    addContactRight();
   } else {
-    addCONTACT();
+    addContactRight();
   }
 });
 
-function addBACK() {
+/*====*/
+meHeaderWrap.addEventListener('click', function () {
+  contact.classList.toggle('contact--none');
+  me.classList.toggle('me--width');
+  meHeaderWrap.classList.toggle('me__header-wrap--width');
+  meListWrap.classList.toggle('me__list-wrap--width');
+});
+
+meHeaderWrap.addEventListener('mouseover', function () {
+  if (meHeaderWrap.classList.contains('me__header-wrap--width') === true) {
+    addBackleft();
+  } else {
+    addMeLeft();
+  }
+});
+
+meHeaderWrap.addEventListener('mouseout', function () {
+  if (meHeaderWrap.classList.contains('me__header-wrap--width') === true) {
+    addMeLeft();
+  } else {
+    addMeLeft();
+  }
+});
+
+
+/*====*/
+function addBackRight() {
   contactHeader.innerHTML = "BACK";
 };
 
-function addCONTACT() {
+function addBackleft() {
+  meHeader.innerHTML = "BACK";
+};
+
+function addContactRight() {
   contactHeader.innerHTML = "CONTACT";
+};
+
+function addMeLeft() {
+  meHeader.innerHTML = "ME";
 };
