f08834e1f47ad8444640610e54b9293bd1901533
Литвиненко Юрий <litvinenko-don@ukr.net>
2020-02-25 22:45:49 +0300

    added logic 'back' to .me__list

--- source/js/script.js
+++ source/js/script.js
@@ -13,58 +13,6 @@ let contactListWrap = document.querySelector('.contact__list-wrap');
 let meListWrap = document.querySelector('.me__list-wrap');
 
 let meList = document.querySelector('.me__list');
-let meLink = document.querySelector('.me__link');
-
-/**======================= */
-/*===клик на CONTACT*/
-contactHeaderWrap.addEventListener('click', function () {
-  me.classList.toggle('me--none');
-  contact.classList.toggle('contact--width');
-  contactHeaderWrap.classList.toggle('contact__header-wrap--width');
-  contactListWrap.classList.toggle('contact__list-wrap--width');
-});
-
-contactHeaderWrap.addEventListener('mouseover', function () {
-  if (contactHeaderWrap.classList.contains('contact__header-wrap--width') === true) {
-    addBackRight();
-    //addBack();
-  } else {
-    addContactRight();
-  }
-});
-
-contactHeaderWrap.addEventListener('mouseout', function () {
-  if (contactHeaderWrap.classList.contains('contact__header-wrap--width') === true) {
-    addContactRight();
-  } else {
-    addContactRight();
-  }
-});
-
-/**======================= */
-/*===клик на ME*/
-meHeaderWrap.addEventListener('click', function () {
-  contact.classList.toggle('contact--none');
-  me.classList.toggle('me--width');
-  meHeaderWrap.classList.toggle('me__header-wrap--width');
-  meListWrap.classList.toggle('me__list-wrap--width');
-});
-
-meHeaderWrap.addEventListener('mouseover', function () {
-  if (meHeaderWrap.classList.contains('me__header-wrap--width') === true) {
-    addBackleft();
-  } else {
-    addMeLeft();
-  }
-});
-
-meHeaderWrap.addEventListener('mouseout', function () {
-  if (meHeaderWrap.classList.contains('me__header-wrap--width') === true) {
-    addMeLeft();
-  } else {
-    addMeLeft();
-  }
-});
 
 
 /**======================= */
@@ -85,40 +33,104 @@ function addMeLeft() {
   meHeader.innerHTML = "ME";
 };
 
-function addBackMeLink() {
-  meLink.innerHTML = "BACK";
+let meWrapper = me.querySelector('.me__wrapper');
+function showMeWrapper() {
+  meHeaderWrap.classList.add('me__header-wrap--none'); //скрыть .me
+  meList.classList.add('me__list--border'); // изменить border
+  meWrapper.classList.add('me__wrapper--width'); //показать .me__wrapper
 };
 
-function addSkillsMeLink() {
-  meLink.innerHTML = "SKILLS";
+function hideMeWrapper() {
+  meHeaderWrap.classList.remove('me__header-wrap--none'); //показать .me
+  meList.classList.remove('me__list--border'); // изменить border в первоначальное состояние
+  meWrapper.classList.remove('me__wrapper--width'); //скрыть .me__wrapper
 };
+
+function removeActive() {
+  meLinkSkills.classList.remove('me__link--active'); //удалить .me__link--active для Skills
+  meLinkCard.classList.remove('me__link--active'); //удалить .me__link--active для Card
+  meLinkEdu.classList.remove('me__link--active'); //удалить .me__link--active для Edu
+};
+
+
+/**======================= */
+/*===клик на CONTACT*/
+contactHeaderWrap.addEventListener('click', function () {
+  me.classList.toggle('me--none');
+  contact.classList.toggle('contact--width');
+  this.classList.toggle('contact__header-wrap--width');
+  contactListWrap.classList.toggle('contact__list-wrap--width');
+});
+
+contactHeaderWrap.addEventListener('mouseover', function () {
+  if (this.classList.contains('contact__header-wrap--width')) {
+    addBackRight();
+  } else {
+    addContactRight();
+  }
+});
+
+contactHeaderWrap.addEventListener('mouseout', function () {
+  if (this.classList.contains('contact__header-wrap--width')) {
+    addContactRight();
+  } else {
+    addContactRight();
+  }
+});
+
+/**======================= */
+/*===клик на ME*/
+meHeaderWrap.addEventListener('click', function () {
+  contact.classList.toggle('contact--none');
+  me.classList.toggle('me--width');
+  this.classList.toggle('me__header-wrap--width');
+  meListWrap.classList.toggle('me__list-wrap--width');
+});
+
+meHeaderWrap.addEventListener('mouseover', function () {
+  if (this.classList.contains('me__header-wrap--width')) {
+    addBackleft();
+  } else {
+    addMeLeft();
+  }
+});
+
+meHeaderWrap.addEventListener('mouseout', function () {
+  if (this.classList.contains('me__header-wrap--width')) {
+    addMeLeft();
+  } else {
+    addMeLeft();
+  }
+});
+
 
 /**======================= */
 /*===клик на .me__link--skills*/
 let meLinkSkills = meList.querySelector('.me__link--skills');
-let meWrapper = me.querySelector('.me__wrapper');
 
-//скрыть .me, изменить border, показать .me__wrapper, добавить .me__link--active
 meLinkSkills.addEventListener('click', function () {
-  meHeaderWrap.classList.add('me__header-wrap--none'); //скрыть .me
-  meList.classList.add('me__list--border'); // изменить border
-  meWrapper.classList.add('me__wrapper--width'); //показать .me__wrapper
-  meLinkSkills.classList.add('me__link--active'); //добавить .me__link--active
-  meLinkCard.classList.remove('me__link--active'); //удалить .me__link--active для Card
-  meLinkEdu.classList.remove('me__link--active'); //удалить .me__link--active для Edu
+  showMeWrapper();
+  removeActive();
+  this.classList.add('me__link--active'); //добавить .me__link--active
+
+  if (this.innerHTML === 'BACK') {
+    hideMeWrapper();
+    this.classList.remove('me__link--active'); //удалить .me__link--active
+    this.innerHTML = 'SKILLS'; //изменить BACK на SKILLS
+  }
 });
 
 //изменить содержимое на BACK
 meLinkSkills.addEventListener('mouseover', function () {
-  if (meLinkSkills.classList.contains('me__link--active') === true) {
-    meLinkSkills.innerHTML = "BACK";
+  if (this.classList.contains('me__link--active') === true) {
+    this.innerHTML = "BACK";
   }
 });
 
 //изменить содержимое на SKILLS
 meLinkSkills.addEventListener('mouseout', function () {
-  if (meLinkSkills.classList.contains('me__link--active') === true) {
-    meLinkSkills.innerHTML = "SKILLS";
+  if (this.classList.contains('me__link--active') === true) {
+    this.innerHTML = "SKILLS";
   }
 });
 
@@ -127,25 +139,28 @@ meLinkSkills.addEventListener('mouseout', function () {
 let meLinkCard = meList.querySelector('.me__link--card-list');
 
 meLinkCard.addEventListener('click', function () {
-  meHeaderWrap.classList.add('me__header-wrap--none'); //скрыть .me
-  meList.classList.add('me__list--border'); // изменить border
-  meWrapper.classList.add('me__wrapper--width'); //показать .me__wrapper
-  meLinkCard.classList.add('me__link--active'); //добавить .me__link--active
-  meLinkSkills.classList.remove('me__link--active'); //удалить .me__link--active для Skills
-  meLinkEdu.classList.remove('me__link--active'); //удалить .me__link--active для Edu
+  showMeWrapper();
+  removeActive();
+  this.classList.add('me__link--active'); //добавить .me__link--active
+
+  if (this.innerHTML === 'BACK') {
+    hideMeWrapper();
+    this.classList.remove('me__link--active'); //удалить .me__link--active
+    this.innerHTML = 'PORTFOLIO'; //изменить BACK на Work
+  }
 });
 
 //изменить содержимое на BACK
 meLinkCard.addEventListener('mouseover', function () {
-  if (meLinkCard.classList.contains('me__link--active') === true) {
-    meLinkCard.innerHTML = "BACK";
+  if (this.classList.contains('me__link--active') === true) {
+    this.innerHTML = "BACK";
   }
 });
 
 //изменить содержимое на PORTFILIO
 meLinkCard.addEventListener('mouseout', function () {
-  if (meLinkCard.classList.contains('me__link--active') === true) {
-    meLinkCard.innerHTML = "PORTFILIO";
+  if (this.classList.contains('me__link--active') === true) {
+    this.innerHTML = "PORTFILIO";
   }
 });
 
@@ -154,25 +169,28 @@ meLinkCard.addEventListener('mouseout', function () {
 let meLinkEdu = meList.querySelector('.me__link--edu');
 
 meLinkEdu.addEventListener('click', function () {
-  meHeaderWrap.classList.add('me__header-wrap--none'); //скрыть .me
-  meList.classList.add('me__list--border'); // изменить border
-  meWrapper.classList.add('me__wrapper--width'); //показать .me__wrapper
-  meLinkEdu.classList.add('me__link--active'); //добавить .me__link--active
-  meLinkSkills.classList.remove('me__link--active'); //удалить .me__link--active для Skills
-  meLinkCard.classList.remove('me__link--active'); //удалить .me__link--active для Card
+  showMeWrapper();
+  removeActive();
+  this.classList.add('me__link--active'); //добавить .me__link--active
+
+  if (this.innerHTML === 'BACK') {
+    hideMeWrapper();
+    this.classList.remove('me__link--active'); //удалить .me__link--active
+    this.innerHTML = 'Education'; //изменить BACK на Education
+  }
 });
 
 //изменить содержимое на BACK
 meLinkEdu.addEventListener('mouseover', function () {
-  if (meLinkEdu.classList.contains('me__link--active') === true) {
-    meLinkEdu.innerHTML = "BACK";
+  if (this.classList.contains('me__link--active') === true) {
+    this.innerHTML = "BACK";
   }
 });
 
 //изменить содержимое на PORTFILIO
 meLinkEdu.addEventListener('mouseout', function () {
-  if (meLinkEdu.classList.contains('me__link--active') === true) {
-    meLinkEdu.innerHTML = "EDUCATION";
+  if (this.classList.contains('me__link--active') === true) {
+    this.innerHTML = "EDUCATION";
   }
 });
 
