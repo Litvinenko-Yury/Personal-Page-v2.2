4895cbfda7e1b834cff2bbe6aad2c778c24c1efe
Литвиненко Юрий <litvinenko-don@ukr.net>
2020-02-29 19:27:51 +0300

    changed zoom for IMG

--- source/js/modal-innerHTML.js
+++ source/js/modal-innerHTML.js
@@ -1,7 +1,70 @@
-/**=== script-innerHTML.js ===*/
+/**=== script modal-innerHTML.js ===*/
 
 /**======================= */
-/*===innerHTML*/
+let eduImgWrap = document.querySelector('.education__img-wrap');
+let modalOverlay = document.querySelector('.modal-overlay');
 
 
+/**===================== */
+function showModal() {
+  modalOverlay.classList.add('modal-overlay--show');
+  //modal.classList.add('modal--show');
+}
+
+function showModalContent1() {
+  modalOverlay.insertAdjacentHTML('afterEnd', "<img class='modal-content' src='img/rastr/Sahara2.jpg' loading='lazy' alt='Certificate №1'>");
+}
+
+function showModalContent2() {
+  modalOverlay.insertAdjacentHTML('afterEnd', "<img class='modal-content' src='img/rastr/taiga.jpg' loading='lazy' alt='Certificate №1'>");
+}
+
+function closeModal() {
+  let modalContent = document.querySelector('.modal-content');
+  modalContent.remove();
+  modalOverlay.classList.remove('modal-overlay--show');
+}
+
+
+/*=============================*/
+/*Показать модальное окно:*/
+/*события по клику на img-preview*/
+eduImgWrap.onclick = function (event) {
+  let target = event.target; // где был клик?
+
+  if (target.classList.contains('education__img--1')) {
+    showModal();
+    showModalContent1();
+  }
+
+  if (target.classList.contains('education__img--2')) {
+    showModal();
+    showModalContent2();
+  }
+};
+
+/*=============================*/
+/*1-Закрыть модальное окно:*/
+/*события по клику на изображение*/
+document.onclick = function (event) {
+  let target = event.target; // где был клик?
+
+  if (target.classList.contains('modal-content')) {
+    closeModal();
+  }
+};
+
+/*2-Закрыть модальное окно:*/
+/*события по клику на затененный фон*/
+modalOverlay.addEventListener("click", function (event) {
+  closeModal();
+});
+
+/*3-Закрыть модальное окно:*/
+/*события по нажатию ESC*/
+window.addEventListener("keydown", function (event) {
+  if (event.keyCode === 27) {
+    closeModal();
+  }
+});
 
