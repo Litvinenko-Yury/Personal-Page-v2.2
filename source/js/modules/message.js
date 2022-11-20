8915f343f2d2b4956b11710291a25231973185e8
Литвиненко Юрий <litvinenko-don@ukr.net>
2022-11-20 17:48:54 +0200

    ref: changes style .about

--- source/js/modules/message.js
+++ source/js/modules/message.js
@@ -1,23 +1,23 @@
 function showMessage() {
-  // const message = document.querySelector('#message');
+  const meHeaderWrap = document.querySelector('.me__header-wrap');
 
-  document.querySelector('.me__header-wrap').addEventListener('mouseover', () => {
+  meHeaderWrap.addEventListener('mouseover', () => {
     if (!document.querySelector('.me__header-wrap').classList.contains('active')) {
       console.log('if !')
       message.classList.add('message--show');
     }
-})
+  })
 
-document.querySelector('.me__header-wrap').addEventListener('mouseout', () => {
-  message.classList.remove('message--show');
-})
-
-document.querySelector('.me__header-wrap').addEventListener('mouseout', () => {
-  if ( document.querySelector('.me__header-wrap').classList.contains('active')) {
-    console.log('if message.classList.contains(active)');
+  meHeaderWrap.addEventListener('mouseout', () => {
     message.classList.remove('message--show');
-  }
-})
+  })
+
+  meHeaderWrap.addEventListener('mouseout', () => {
+    if (document.querySelector('.me__header-wrap').classList.contains('active')) {
+      console.log('if message.classList.contains(active)');
+      message.classList.remove('message--show');
+    }
+  })
 }
 
 export default showMessage;
